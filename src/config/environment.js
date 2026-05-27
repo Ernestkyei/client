bdd25948227411612a64f69c5a85fd1c89b993b5
ErnestKyei <kyeiernest86@gmail.com>
2026-05-27 15:31:50 +0000

    new updated

--- src/config/environment.js
+++ src/config/environment.js
@@ -1,5 +1,5 @@
 export const config = {
-  API_URL: 'http://localhost:5000/api',
+  API_URL: import.meta.env.VITE_API_URL,
   PAYSTACK_PUBLIC_KEY: 'pk_test_55cf4aea5f94a74c7bf928825a8ccf33bd5fc82d',
-  MODE: 'development'
+  MODE: import.meta.env.MODE
 };
